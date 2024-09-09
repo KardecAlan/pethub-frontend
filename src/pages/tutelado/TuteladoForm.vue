9baa3f09d8c9334cb12084dd781e83dc02b1433d
Alan Kardec <alankardec05@gmail.com>
2024-09-09 10:37:49 -0300

    feat: Implementando o TuteladoForm

---
+++ src/pages/tutelado/TuteladoForm.vue
@@ -0,0 +1,79 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const tuteladoForm = ref(
+  {
+    nome: '',
+    sexo: '',
+    especie: '',
+    idade: '',
+    peso: '',
+    tutorId: '',
+  },
+);
+
+</script>
+
+<template>
+  <q-page-container>
+    <q-page>
+      <q-card>
+        <q-card-section>
+          <h2>Cadastrar Tutelado</h2>
+        </q-card-section>
+
+        <q-card-section>
+          <q-form class="row">
+            <q-input
+              v-model="tuteladoForm.nome"
+              label="Nome"
+              class="col-3"
+              outlined
+            />
+            <q-input
+              v-model="tuteladoForm.especie"
+              label="Especie"
+              class="col-3"
+              outlined
+            />
+            <q-input
+              v-model="tuteladoForm.sexo"
+              label="Sexo"
+              class="col-3"
+              outlined
+            />
+            <q-input
+              v-model="tuteladoForm.idade"
+              label="Idade"
+              class="col-3"
+              outlined
+            />
+            <q-input
+              v-model="tuteladoForm.peso"
+              label="Peso"
+              class="col-3"
+              outlined
+            />
+            <q-input
+              v-model="tuteladoForm.tutorId"
+              label="Tutor"
+              class="col-3"
+              outlined
+            />
+            <q-btn
+              type="submit"
+              label="Salvar"
+              color="primary"
+            />
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </q-page>
+  </q-page-container>
+
+</template>
+
+<style scoped>
+
+</style>
