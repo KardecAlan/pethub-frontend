738d4f9f359c299d0e603893868c43afdc3dad5c
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2024-09-16 12:31:20 -0300

    refactort: use table view as component

---
+++ src/components/tableView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps(['data', 'columns']);
+
+</script>
+
+<template>
+
+  <q-card class="q-ma-md">
+    <q-markup-table>
+      <thead>
+        <tr>
+          <th class="text-center" v-for="col in props.columns">{{ col.label }}</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="data in props.data">
+          <td class="text-center" v-for="col in props.columns">{{ data[col.field] }}</td>
+          <q-td>
+            <q-btn flat icon="edit" />
+            <q-btn flat icon="delete" />
+          </q-td>
+        </tr>
+      </tbody>
+    </q-markup-table>
+  </q-card>
+</template>
